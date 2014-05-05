7bb05abf8f47fb06c5cc57dee8a8ac2e6fecd469
Peter Harrison <peter.harrison@helicron.net>
2014-05-05 21:17:14 +0100

    Corrects missing states for 180 degree turns

--- makepath.c
+++ makepath.c
@@ -129,8 +129,7 @@ void makeSimplePath(const char * s) {
           x = 2;
           state = PathOrtho_F;
         } else if (c == 'R') {
-          emitCommand(CMD_ERROR_02);
-          state = PathStop;
+          state = PathOrtho_RR;
         } else if (c == 'L') {
           emitCommand(SS90ER);
           emitCommand(FWD1);
@@ -154,14 +153,53 @@ void makeSimplePath(const char * s) {
           emitCommand(FWD1);
           state = PathOrtho_R;
         } else if (c == 'L') {
-          emitCommand(CMD_ERROR_03);
-          state = PathStop;
+          state = PathOrtho_LL;
         } else if (c == 'S') {
           emitCommand(SS90EL);
           emitCommand(FWD1);
           state = PathStop;
         } else {
           emitCommand(CMD_ERROR_03);
+          state = PathStop;
+        }
+        break;
+      case PathOrtho_LL:
+        if (c == 'F') {
+          emitCommand(SS180L);
+          x = 2;
+          state = PathOrtho_F;
+        } else if (c == 'R') {
+          emitCommand(CMD_ERROR_05);
+          state = PathStop;
+        } else if (c == 'L') {
+          emitCommand(CMD_ERROR_05);
+          state = PathStop;
+        } else if (c == 'S') {
+          emitCommand(SS180L);
+          emitCommand(FWD1);
+          state = PathStop;
+        } else {
+          emitCommand(CMD_ERROR_05);
+          state = PathStop;
+        }
+        break;
+      case PathOrtho_RR:
+        if (c == 'F') {
+          emitCommand(SS180R);
+          x = 2;
+          state = PathOrtho_F;
+        } else if (c == 'R') {
+          emitCommand(CMD_ERROR_04);
+          state = PathStop;
+        } else if (c == 'L') {
+          emitCommand(CMD_ERROR_04);
+          state = PathStop;
+        } else if (c == 'S') {
+          emitCommand(SS180R);
+          emitCommand(FWD1);
+          state = PathStop;
+        } else {
+          emitCommand(CMD_ERROR_04);
           state = PathStop;
         }
         break;
